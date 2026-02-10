617bc4e034af96d65a54d0ac82182c7cec8bb7d2
seedre <taocripto@gmail.com>
2026-02-10 23:33:30 +0100

    app-notes

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,19 +1,18 @@
 import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { fetchNoteById } from "@/lib/api";
+import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
 
-export default async function NotePage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-
+export default async function NotesPage() {
   const client = new QueryClient();
 
   await client.prefetchQuery({
-    queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id),
+    queryKey: ["notes", 1, ""], 
+    queryFn: () =>
+      fetchNotes({
+        page: 1,
+        perPage: 12,
+        search: "",
+      }),
   });
 
   return (
@@ -22,3 +21,42 @@ export default async function NotePage({
     </HydrationBoundary>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
+// import { fetchNoteById } from "@/lib/api";
+// import NotesClient from "./Notes.client";
+
+// export default async function NotePage({
+//   params,
+// }: {
+//   params: Promise<{ id: string }>;
+// }) {
+//   const { id } = await params;
+
+//   const client = new QueryClient();
+
+//   await client.prefetchQuery({
+//     queryKey: ["note", id],
+//     queryFn: () => fetchNoteById(id),
+//   });
+
+//   return (
+//     <HydrationBoundary state={dehydrate(client)}>
+//       <NotesClient />
+//     </HydrationBoundary>
+//   );
+// }
--- types/note.ts
+++ types/note.ts
@@ -1,17 +1,19 @@
-export interface Note {
-
-  id: string;
-  title: string;
-  content: string;
-  tag: string;
-  createdAt: string;
-  updatedAt: string;
-
-}
-
 export type NoteTag =
   | "Todo"
   | "Work"
   | "Personal"
   | "Meeting"
   | "Shopping";
+
+
+export interface Note {
+
+  id: string;
+  title: string;
+  content: string;
+  tag: NoteTag;
+  createdAt: string;
+  updatedAt: string;
+
+}
+
